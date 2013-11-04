5e834ca83aa257d5cc15a2b1e83a7d4bb3f2d934
Dmytro Vasylenko <dmytro.vasylenko@eltrino.com>
2013-11-04 17:59:37 +0200

    Initial working modification

--- toggl_to_jira.rb
+++ toggl_to_jira.rb
@@ -3,10 +3,10 @@ require 'net/https'
 require 'uri'
 require 'cgi'
 require 'rubygems'
-require 'active_support/all'
 require 'json'
-require 'jira4r'
 require 'yaml'
+require 'time'
+require 'rest_client'
 
 def req_toggl(uri, token)
   http = Net::HTTP.new(uri.host, uri.port)
@@ -34,7 +34,10 @@ CONFIG = YAML.load_file(File.expand_path(File.dirname(__FILE__)) + '/config.yml'
 CONFIG['start_time'] ||= Time.now.beginning_of_day.iso8601
 CONFIG['start_time'] = CONFIG['start_time'].days.ago.iso8601 if CONFIG['start_time'].is_a?(Fixnum)
 uri = URI.parse("https://www.toggl.com/api/v8/time_entries?start_date=#{CGI.escape(CONFIG['start_time'])}" +
-                    (CONFIG['end_time'].blank? ? '' : "&end_date=#{CGI.escape(CONFIG['end_time'])}"))
+                    (CONFIG['end_time'].empty? ? '' : "&end_date=#{CGI.escape(CONFIG['end_time'])}"))
+
+puts "https://www.toggl.com/api/v8/time_entries?start_date=#{CGI.escape(CONFIG['start_time'])}" +
+         (CONFIG['end_time'].empty? ? '' : "&end_date=#{CGI.escape(CONFIG['end_time'])}")
 
 puts "Connecting to toggl starting from #{CONFIG['start_time']}"
 entries = req_toggl(uri, CONFIG['toggl_key'])
@@ -43,23 +46,9 @@ puts "Got #{entries.length} entries from toggl"
 IMPORTED_FILE = File.expand_path(File.dirname(__FILE__)) + '/imported.yml'
 imported = (YAML.load_file(IMPORTED_FILE) rescue [])
 
-puts 'Connecting to JIRA'
-begin
-  logger = Logger.new(STDERR)
-  logger.sev_threshold = Logger::WARN
-  jira = Jira4R::JiraTool.new(2, CONFIG['jira_url'])
-  jira.logger = logger
-  jira.login CONFIG['jira_user'], CONFIG['jira_pass']
-rescue
-  puts 'Failed to login to JIRA'
-  puts $!
-  exit(1)
-end
-puts "Successfully login to JIRA as #{CONFIG['jira_user']}"
-
 entries.each do |entry|
   id = entry['id']
-  start = Time.iso8601(entry['start'])
+  start = Time.parse(entry['start'])
   duration = entry['duration'].to_i
   desc = entry['description'] || ''
   if entry['pid']
@@ -69,6 +58,7 @@ entries.each do |entry|
     desc += " #{project['data']['name']}" if project['data']['name']
   end
   jira_key = $1 if desc =~ /([A-Z]+-\d+)/
+  jira_url = CONFIG['jira_url'] + "/rest/api/2/issue/#{jira_key}/worklog"
 
   if imported.include?(id)
     puts "Skip #{jira_key} '#{desc}' as it was already imported"
@@ -76,21 +66,46 @@ entries.each do |entry|
     puts "Skip #{jira_key} '#{desc}' as it's still running"
   elsif entry['duration'].to_i < 60
     puts "Skip #{jira_key} '#{desc}' as its duration is less than 1 minute"
-  elsif jira_key.blank?
+  elsif jira_key.nil?
     puts "Skip #{jira_key} '#{desc}' as it doesn't have a JIRA ticket key"
   else
-    remoteWorklog = Jira4R::V2::RemoteWorklog.new
-    remoteWorklog.comment = "#{desc} , generated from toggl_to_jira script"
-    remoteWorklog.startDate = start
-    remoteWorklog.timeSpent = "#{(duration / 60.0).round}m"
-    puts "Add worklog #{remoteWorklog.timeSpent.rjust(4)} from #{remoteWorklog.startDate.localtime.strftime('%b %e, %l:%M %p')} to ticket #{jira_key} #{desc}"
+    comment = "#{desc} , generated from toggl_to_jira script"
+    startDate = start.localtime.strftime('%b %e, %l:%M %p')
+    timeSpent = (duration / 60.0).round
+    puts "Add worklog #{timeSpent} from #{startDate} to ticket #{jira_key} #{desc}"
+    #jira rest request here
+
+    worklog = {
+        :comment => comment,
+        :started => (start.strftime "%FT%T.%L%z"),
+        :timeSpent => "#{timeSpent}m"
+    }
+
+    request = RestClient::Request.new(
+        :url => jira_url,
+        :method => :post,
+        :user => CONFIG['jira_user'],
+        :password => CONFIG['jira_pass'],
+        :headers => {
+            :accept => "application/json",
+            :content_type => "application/json"
+        },
+        :payload => worklog.to_json
+    )
+
     begin
-      jira.addWorklogAndAutoAdjustRemainingEstimate(jira_key, remoteWorklog)
+      response = JSON.parse(request.execute)
+
+      if !response['id']
+        raise("Bad response from JIRA")
+      end
+
       imported.push id
       imported.shift if imported.size > 500 # avoid imported list increasing infinitely
       File.open(IMPORTED_FILE, 'w') { |f| f.write(imported.to_yaml) }
-    rescue SOAP::Error => error
-      STDERR.puts 'Error: ' + error
+
+    rescue
+      puts $!
     end
   end
 end
